a17a1a2af69dce1c4b00c3fc8d7bdb9e1a1264fe
CristianVargas03 <65677401+CristianVargas03@users.noreply.github.com>
2020-10-13 16:20:39 -0400

    Ajustes a Controller

---
+++ Proyecto_Grupal/Controller/Categoria_Ctrl.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controller
+{
+    class Categoria_Ctrl
+    {
+        public int ID
+        {
+            get
+            {
+                return ID;
+            }
+            set
+            {
+                ID = value;
+
+            }
+        }
+
+        public int Nombre
+        {
+            get
+            {
+                return Nombre;
+            }
+            set
+            {
+                Nombre = value;
+
+            }
+        }
+
+        public int Estado
+        {
+            get
+            {
+                return Estado;
+            }
+            set
+            {
+                Estado = value;
+
+            }
+        }
+
+        public void Read()
+        {
+           
+        }
+
+        public void Create()
+        {
+            
+        }
+
+        public void Update()
+        {
+           
+        }
+
+        public void Delete()
+        {
+           
+        }
+    }
+}
