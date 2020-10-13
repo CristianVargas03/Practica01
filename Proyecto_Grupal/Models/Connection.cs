2d5579c836e2388c92ae166e9942a5bbc609f7ac
OnilyValera <70966644+OnilyValera@users.noreply.github.com>
2020-10-13 15:32:04 -0400

    Conexion

---
+++ Proyecto_Grupal/Models/Connection.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Models
+{
+    class Connection
+    {
+        SqlConnection Conexion = new SqlConnection("Data source= DESKTOP-KL13RAS; Initial Catalog= Practica1; Integrated Security=True");
+        public SqlConnection AbrirConexion()
+        {
+            if (Conexion.State == ConnectionState.Closed)
+                Conexion.Open();
+            return Conexion;
+        }
+        public SqlConnection CerrarConexion()
+        {
+            if (Conexion.State == ConnectionState.Open)
+                Conexion.Close();
+            return Conexion;
+
+        }
+
+        public void Prueba()
+        {
+            try
+            {
+                Conexion.Open();
+                Console.WriteLine("Conexion Exitosa");
+            }
+            catch
+            {
+                Console.WriteLine("Conexion Fallida");
+            }
+            Conexion.Close();
+        }
+
+
+
+
+    }
+}
